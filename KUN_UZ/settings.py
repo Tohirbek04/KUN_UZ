fdd66147fe1249dcd50ccb7a23521148f01cfb3c
Tohirbek04 <tohirbekakhmedov1310@gmail.com>
2024-04-03 02:38:34 +0500

    static file

--- KUN_UZ/settings.py
+++ KUN_UZ/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v!udrb9^q@$b4i9-q+1q9$3_yff&i!zx47e^q9h^qqi7)_k1@l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static/']
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
